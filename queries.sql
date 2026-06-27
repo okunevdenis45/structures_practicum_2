73332815dc99eef228b07a15a2e0ed8c8f83f59d
okunevdenis45 <okunevdenis45@gmail.com>
2026-06-27 13:18:36 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,19 @@
+-- Запрос: Кто сейчас внутри зоны А
+SELECT e.full_name, e.position, e.department
+FROM employees e
+JOIN passes p ON e.employee_id = p.employee_id
+JOIN (
+    SELECT pass_id, MAX(timestamp) AS last_event_time
+    FROM history_pass
+    WHERE zone_id = 1
+      AND notes = 'Успешно'
+    GROUP BY pass_id
+) hp_last ON p.pass_id = hp_last.pass_id
+JOIN history_pass hp ON hp_last.pass_id = hp.pass_id
+    AND hp_last.last_event_time = hp.timestamp
+WHERE hp.direction = 'in';
+
+
 -- запрос: Просроченные пропуска
 SELECT
     e.full_name,
@@ -23,21 +39,6 @@ WHERE hp.reason_id = 3
 GROUP BY p.pass_id, e.full_name
 ORDER BY attempts_count DESC;
 
-
--- Запрос: Кто сейчас внутри зоны А
-SELECT e.full_name, e.position, e.department
-FROM employees e
-JOIN passes p ON e.employee_id = p.employee_id
-JOIN (
-    SELECT pass_id, MAX(timestamp) AS last_event_time
-    FROM history_pass
-    WHERE zone_id = 1
-      AND notes = 'Успешно'
-    GROUP BY pass_id
-) hp_last ON p.pass_id = hp_last.pass_id
-JOIN history_pass hp ON hp_last.pass_id = hp.pass_id
-    AND hp_last.last_event_time = hp.timestamp
-WHERE hp.direction = 'in';
 
 
 
